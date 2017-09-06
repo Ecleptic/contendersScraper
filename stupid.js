19d275c2f12dc9b86eeaadb367bcb622dc5ba6c2
Cameron Green <cgree78@gmail.com>
2017-09-06 08:15:17 -0500

    added stupid.js which is what the JSNO currently looks like

---
+++ stupid.js
@@ -0,0 +1,493 @@
+[
+    {
+        Team1: 'Misfits',
+        Team1Score: '4',
+        Team2: 'Cloud9',
+        Team2Score: '0',
+        gameDate: 'AUG 19',
+        matchTime: '5:00 PM EST', //should be 12:00pm CDT
+        teamWin: 'team1'
+    }, {
+        Team1: 'Team Gigantti',
+        Team1Score: '3',
+        Team2: 'eUnited',
+        Team2Score: '1',
+        gameDate: 'AUG 19',
+        matchTime: '7:00 PM EST', //2:00 CDT
+        teamWin: 'team1'
+    }, {
+        Team1: 'EnVyUS',
+        Team1Score: '4',
+        Team2: 'Immortals',
+        Team2Score: '0',
+        gameDate: 'AUG 19',
+        matchTime: '9:15 PM EST', //4:15
+        teamWin: 'team1'
+    }, {
+        Team1: 'EnVision',
+        Team1Score: '3',
+        Team2: 'Rogue',
+        Team2Score: '1',
+        gameDate: 'AUG 19',
+        matchTime: '11:15 PM EST', //6:15 cdt
+        teamWin: 'team1'
+    }, {
+        Team1: 'Singularity',
+        Team1Score: '0',
+        Team2: '123',
+        Team2Score: '4',
+        gameDate: 'AUG 20',
+        matchTime: '5:00 PM EST',
+        teamWin: 'team2'
+    }, {
+        Team1: 'GamersOrigin',
+        Team1Score: '3',
+        Team2: 'Bazooka Puppiez',
+        Team2Score: '1',
+        gameDate: 'AUG 20',
+        matchTime: '7:00 PM EST', //2:00
+        teamWin: 'team1'
+    }, {
+        Team1: 'Kungarna',
+        Team1Score: '4',
+        Team2: 'Detroit Renegades',
+        Team2Score: '0',
+        gameDate: 'AUG 20',
+        matchTime: '9:15 PM EST',
+        teamWin: 'team1'
+    }, {
+        Team1: 'FaZe Clan',
+        Team1Score: '3',
+        Team2: 'FNRGFE',
+        Team2Score: '1',
+        gameDate: 'AUG 20',
+        matchTime: '11:15 PM EST',  //should be 6:15 cdt
+        teamWin: 'team1'
+    }, {
+        Team1: '123',
+        Team1Score: '4',
+        Team2: 'Bazooka Puppiez',
+        Team2Score: '0',
+        gameDate: 'AUG 26',
+        matchTime: '5:00 PM EST', //12:00 CDT
+        teamWin: 'team1'
+    }, {
+        Team1: 'Singularity',
+        Team1Score: '0',
+        Team2: 'GamersOrigin',
+        Team2Score: '4',
+        gameDate: 'AUG 26',
+        matchTime: '7:00 PM EST', // 2:00
+        teamWin: 'team2'
+    }, {
+        Team1: 'Detroit Renegades',
+        Team1Score: '1',
+        Team2: 'Rogue',
+        Team2Score: '3',
+        gameDate: 'AUG 26',
+        matchTime: '9:15 PM EST',
+        teamWin: 'team2'
+    }, {
+        Team1: 'Kungarna',
+        Team1Score: '3',
+        Team2: 'FNRGFE',
+        Team2Score: '1',
+        gameDate: 'AUG 26',
+        matchTime: '11:15 PM EST',
+        teamWin: 'team1'
+    }, {
+        Team1: 'eUnited',
+        Team1Score: '2',
+        Team2: 'Cloud9',
+        Team2Score: '3',
+        gameDate: 'AUG 27',
+        matchTime: '5:00 PM EST', //12:00
+        teamWin: 'team2'
+    }, {
+        Team1: 'Misfits',
+        Team1Score: '4',
+        Team2: 'Team Gigantti',
+        Team2Score: '0',
+        gameDate: 'AUG 27',
+        matchTime: '7:00 PM EST',
+        teamWin: 'team1'
+    }, {
+        Team1: 'EnVyUS',
+        Team1Score: '3',
+        Team2: 'FaZe Clan',
+        Team2Score: '1',
+        gameDate: 'AUG 27',
+        matchTime: '9:15 PM EST',
+        teamWin: 'team1'
+    }, {
+        Team1: 'EnVision',
+        Team1Score: '3',
+        Team2: 'Immortals',
+        Team2Score: '1',
+        gameDate: 'AUG 27',
+        matchTime: '11:15 PM EST', // 6:15
+        teamWin: 'team1'
+    }, {
+        Team1: 'Immortals',
+        Team1Score: '2',
+        Team2: 'FaZe Clan',
+        Team2Score: '3',
+        gameDate: 'SEP 1',
+        matchTime: '12:00 AM EST', // should be 7:00PM CDT
+        teamWin: 'team2'
+    }, {
+        Team1: 'Detroit Renegades',
+        Team1Score: '0',
+        Team2: 'EnVision',
+        Team2Score: '4',
+        gameDate: 'SEP 1',
+        matchTime: '2:00 AM EST', //9:00 pm CDT
+        teamWin: 'team2'
+    }, {
+        Team1: '123',
+        Team1Score: '3',
+        Team2: 'eUnited',
+        Team2Score: '1',
+        gameDate: 'Sep 2',
+        matchTime: '5:00 PM EST',
+        teamWin: 'team1'
+    }, {
+        Team1: 'Bazooka Puppiez',
+        Team1Score: '1',
+        Team2: 'Cloud9',
+        Team2Score: '3',
+        gameDate: 'Sep 2',
+        matchTime: '7:00 PM EST',
+        teamWin: 'team2'
+    }, {
+        Team1: 'FNRGFE',
+        Team1Score: '3',
+        Team2: 'Rogue',
+        Team2Score: '2',
+        gameDate: 'Sep 2',
+        matchTime: '9:15 PM EST',
+        teamWin: 'team1'
+    }, {
+        Team1: 'Kungarna',
+        Team1Score: '0',
+        Team2: 'EnVyUS',
+        Team2Score: '4',
+        gameDate: 'Sep 2',
+        matchTime: '11:15 PM EST',
+        teamWin: 'team2'
+    }, {
+        Team1: 'Immortals',
+        Team1Score: '-',
+        Team2: 'Rogue',
+        Team2Score: '-',
+        gameDate: 'SEP 3',
+        matchTime: '9:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Detroit Renegades',
+        Team1Score: '-',
+        Team2: 'FaZe Clan',
+        Team2Score: '-',
+        gameDate: 'SEP 3',
+        matchTime: '11:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Singularity',
+        Team1Score: '0',
+        Team2: 'Misfits',
+        Team2Score: '0',
+        gameDate: 'SEP 3',
+        matchTime: '5:00 PM EST',
+        teamWin: 'team2'
+    }, {
+        Team1: 'GamersOrigin',
+        Team1Score: '-',
+        Team2: 'Team Gigantti',
+        Team2Score: '-',
+        gameDate: 'SEP 3',
+        matchTime: '7:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Immortals',
+        Team1Score: '-',
+        Team2: 'Rogue',
+        Team2Score: '-',
+        gameDate: 'SEP 8',
+        matchTime: '9:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Detroit Renegades',
+        Team1Score: '-',
+        Team2: 'FaZe Clan',
+        Team2Score: '-',
+        gameDate: 'SEP 8',
+        matchTime: '11:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Singularity',
+        Team1Score: '0',
+        Team2: 'Misfits',
+        Team2Score: '0',
+        gameDate: 'SEP 9',
+        matchTime: '5:00 PM EST',
+        teamWin: 'team2'
+    }, {
+        Team1: 'GamersOrigin',
+        Team1Score: '-',
+        Team2: 'Team Gigantti',
+        Team2Score: '-',
+        gameDate: 'SEP 9',
+        matchTime: '7:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Immortals',
+        Team1Score: '-',
+        Team2: 'Rogue',
+        Team2Score: '-',
+        gameDate: 'SEP 9',
+        matchTime: '9:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Detroit Renegades',
+        Team1Score: '-',
+        Team2: 'FaZe Clan',
+        Team2Score: '-',
+        gameDate: 'SEP 9',
+        matchTime: '11:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: '123',
+        Team1Score: '-',
+        Team2: 'Team Gigantti',
+        Team2Score: '-',
+        gameDate: 'SEP 10',
+        matchTime: '4:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Singularity',
+        Team1Score: '-',
+        Team2: 'Bazooka Puppiez',
+        Team2Score: '-',
+        gameDate: 'SEP 10',
+        matchTime: '6:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Singularity',
+        Team1Score: '-',
+        Team2: 'eUnited',
+        Team2Score: '-',
+        gameDate: 'SEP 10',
+        matchTime: '5:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Misfits',
+        Team1Score: '-',
+        Team2: 'GamersOrigin',
+        Team2Score: '-',
+        gameDate: 'SEP 10',
+        matchTime: '7:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Detroit Renegades',
+        Team1Score: '-',
+        Team2: 'EnVyUS',
+        Team2Score: '-',
+        gameDate: 'SEP 15',
+        matchTime: '9:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'FaZe Clan',
+        Team1Score: '-',
+        Team2: 'Rogue',
+        Team2Score: '-',
+        gameDate: 'SEP 15',
+        matchTime: '11:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Misfits',
+        Team1Score: '-',
+        Team2: 'Bazooka Puppiez',
+        Team2Score: '-',
+        gameDate: 'SEP 16',
+        matchTime: '5:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Team Gigantti',
+        Team1Score: '-',
+        Team2: 'Cloud9',
+        Team2Score: '-',
+        gameDate: 'SEP 16',
+        matchTime: '7:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Kungarna',
+        Team1Score: '-',
+        Team2: 'EnVision',
+        Team2Score: '-',
+        gameDate: 'SEP 16',
+        matchTime: '9:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Immortals',
+        Team1Score: '-',
+        Team2: 'FNRGFE',
+        Team2Score: '-',
+        gameDate: 'SEP 16',
+        matchTime: '11:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'EnVyUS',
+        Team1Score: '-',
+        Team2: 'FNRGFE',
+        Team2Score: '-',
+        gameDate: 'SEP 17',
+        matchTime: '12:00 AM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Kungarna',
+        Team1Score: '-',
+        Team2: 'Rogue',
+        Team2Score: '-',
+        gameDate: 'SEP 17',
+        matchTime: '2:00 AM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'eUnited',
+        Team1Score: '-',
+        Team2: 'Bazooka Puppiez',
+        Team2Score: '-',
+        gameDate: 'SEP 17',
+        matchTime: '5:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: '123',
+        Team1Score: '-',
+        Team2: 'Misfits',
+        Team2Score: '-',
+        gameDate: 'SEP 17',
+        matchTime: '7:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'EnVision',
+        Team1Score: '-',
+        Team2: 'FNRGFE',
+        Team2Score: '-',
+        gameDate: 'SEP 22',
+        matchTime: '9:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Detroit Renegades',
+        Team1Score: '-',
+        Team2: 'Immortals',
+        Team2Score: '-',
+        gameDate: 'SEP 22',
+        matchTime: '11:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'GamersOrigin',
+        Team1Score: '-',
+        Team2: 'Cloud9',
+        Team2Score: '-',
+        gameDate: 'SEP 23',
+        matchTime: '5:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Singularity',
+        Team1Score: '-',
+        Team2: 'Team Gigantti',
+        Team2Score: '-',
+        gameDate: 'SEP 23',
+        matchTime: '7:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Kungarna',
+        Team1Score: '-',
+        Team2: 'FaZe Clan',
+        Team2Score: '-',
+        gameDate: 'SEP 23',
+        matchTime: '9:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'EnVision',
+        Team1Score: '-',
+        Team2: 'EnVyUS',
+        Team2Score: '-',
+        gameDate: 'SEP 23',
+        matchTime: '11:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: '123',
+        Team1Score: '-',
+        Team2: 'Cloud9',
+        Team2Score: '-',
+        gameDate: 'SEP 24',
+        matchTime: '4:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Team Gigantti',
+        Team1Score: '-',
+        Team2: 'Bazooka Puppiez',
+        Team2Score: '-',
+        gameDate: 'SEP 24',
+        matchTime: '6:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'GamersOrigin',
+        Team1Score: '-',
+        Team2: 'eUnited',
+        Team2Score: '-',
+        gameDate: 'SEP 24',
+        matchTime: '5:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Singularity',
+        Team1Score: '-',
+        Team2: 'Cloud9',
+        Team2Score: '-',
+        gameDate: 'SEP 24',
+        matchTime: '7:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'EnVyUS',
+        Team1Score: '-',
+        Team2: 'Rogue',
+        Team2Score: '-',
+        matchTime: '9:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'EnVision',
+        Team1Score: '-',
+        Team2: 'FaZe Clan',
+        Team2Score: '-',
+        matchTime: '11:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: '123',
+        Team1Score: '-',
+        Team2: 'GamersOrigin',
+        Team2Score: '-',
+        matchTime: '5:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Misfits',
+        Team1Score: '-',
+        Team2: 'eUnited',
+        Team2Score: '-',
+        matchTime: '7:00 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Detroit Renegades',
+        Team1Score: '-',
+        Team2: 'FNRGFE',
+        Team2Score: '-',
+        matchTime: '9:15 PM EST',
+        teamWin: '-'
+    }, {
+        Team1: 'Kungarna',
+        Team1Score: '-',
+        Team2: 'Immortals',
+        Team2Score: '-',
+        matchTime: '11:15 PM EST',
+        teamWin: '-'
+    }
+]
